ede5990782ad69c05d1604f3a10037ee0064028a
Efrain Gonzalez <efrain@juniper.net>
2019-12-23 00:53:35 +0100

    testing

--- tests/api-server/app/main.py
+++ tests/api-server/app/main.py
@@ -9,4 +9,4 @@ def test_json():
 # Run in HTTP
 if __name__ == "__main__":
     # Only for debugging while developing
-	app.run(host='0.0.0.0', debug=True, port='5000')  
+	app.run(host='0.0.0.0', debug=True, port='80')  
