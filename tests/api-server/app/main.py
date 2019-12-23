0eadf142db406107ec6e93672828f8b811dbfee1
Efrain Gonzalez <efrain@juniper.net>
2019-12-23 05:51:22 +0100

    testing

--- tests/api-server/app/main.py
+++ tests/api-server/app/main.py
@@ -1,12 +1,14 @@
 from flask import Flask
-
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World from Flask"
 
 @app.route('/json', methods=['POST', 'GET'])
 def test_json():
     return '{"code": 1, "message": "Hello, World!" }'
-   
-# Run in HTTP
+
 if __name__ == "__main__":
     # Only for debugging while developing
-	app.run(host='0.0.0.0', debug=True, port='80')  
+    app.run(host='0.0.0.0', debug=True, port=80)
--- tests/test_mock_service.py
+++ tests/test_mock_service.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_mock_service():
-    url = 'http://127.0.0.1:5000/json'    
+    url = 'http://127.0.0.1:80/json'    
     resp = requests.get(url)           
     assert resp.status_code == 200
     assert resp.json()["code"] == 1
